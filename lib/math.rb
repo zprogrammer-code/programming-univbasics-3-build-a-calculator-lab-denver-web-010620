ad4f9a433a09b28cab9832ec5b14b867bb59577b
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-19 19:48:19 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,15 +3,15 @@ num1 + num2
 end
 
 def subtraction(num1, num2)
-
+num1 - num2
 end
 
 def division(num1, num2)
-
+num1/num2
 end
 
 def multiplication(num1, num2)
-
+num1*num2
 end
 
 def modulo(num1, num2)
